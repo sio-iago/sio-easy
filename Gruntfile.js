ae70607e1a1000ff843d34e06b5417f0137b00f1
Iago Oliveira <silva.io@outlook.com>
2016-05-30 04:05:36 -0400

    Configuration to uglify sio-easy.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,24 @@ module.exports = function(grunt) {
                     'dist/sio-easy.min.css': ['css/sio-easy.css']
                 }
             }
+        },
+        uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: {
+                    'dist/sio-easy.min.js': ['js/sio-easy.js']
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Default task(s).
-    grunt.registerTask('default', ['cssmin']);
+    grunt.registerTask('default', ['cssmin','uglify']);
 
 }
 
