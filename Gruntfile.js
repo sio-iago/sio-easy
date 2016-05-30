11b18a441689e0cdd069c7edcf8c3fdd413be3f4
Iago Oliveira <silva.io@outlook.com>
2016-05-30 04:15:55 -0400

    Added sass to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         cssmin: {
             options: {
                 shorthandCompacting: false,
@@ -16,19 +17,31 @@ module.exports = function(grunt) {
             options: {
                 mangle: false
             },
-            my_target: {
+            target: {
                 files: {
                     'dist/sio-easy.min.js': ['js/sio-easy.js']
                 }
+            }
+        },
+        sass: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'css',
+                    src: ['*.scss'],
+                    dest: 'dist',
+                    ext: '.css'
+                }]
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
     // Default task(s).
-    grunt.registerTask('default', ['cssmin','uglify']);
+    grunt.registerTask('default', ['sass','uglify','cssmin']);
 
 }
 
